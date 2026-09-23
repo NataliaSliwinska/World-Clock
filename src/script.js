c6a07fe2054297a6f8afde8ee5e94c5bf890ba8c
NataliaSliwinska <natalia.k.sliwinska@gmail.com>
2026-09-23 14:27:44 +0100

    added js to the project

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let warsawElement = document.querySelector("#warsaw");
+  let warsawDateElement = warsawElement.querySelector(".date");
+  let warsawTimeElement = warsawElement.querySelector(".time");
+  let warsawTime = moment().tz("Europe/Warsaw");
+  warsawDateElement.innerHTML = warsawTime.format("MMMM Do YYYY");
+  warsawTimeElement.innerHTML = warsawTime.format(
+    "H:mm:ss [<small>]A[</small>]",
+  );
 
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "H:mm:ss [<small>]A[</small>]",
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
